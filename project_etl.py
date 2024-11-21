c6be780e7d9cd1041e999b89f998fcc729b18789
yaspra <pranati.nov22@gmail.com>
2024-11-20 22:46:30 -0500

    As of EOD 11_20_2024

--- project_etl.py
+++ project_etl.py
@@ -2,12 +2,12 @@
 This Script is the main python script to extract, load and create the output for the website "Books To Scrape". 
 
 """
-
+"""
 #step1 :Install requests and beautiful soup.
 
-pip install requests
-pip install bs4
-
+install requests
+install bs4
+"""
 #step2 : Using the pip -request read the page "books to scrape. Print it to validate
 
 import requests
